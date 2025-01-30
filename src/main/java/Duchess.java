45c760d69cbabb5645b164f177e91bd42bea216e
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-01-31 00:44:02 +0800

    Changed dukes name to duchess

---
+++ src/main/java/Duchess.java
@@ -0,0 +1,10 @@
+public class Duchess {
+    public static void main(String[] args) {
+        String logo = " ____              _\n"
+                + "|  _ \\ _   _  ____| |_    ___  ___  ___\n"
+                + "| | | | | | |/ ___|  _ \\ / _ \\/ __|/ __|\n"
+                + "| |_| | |_| | |___| | | |  __/\\__ \\\\__ \\\n"
+                + "|____/ \\__,_|\\____|_| |_|\\___||___/|___/\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}

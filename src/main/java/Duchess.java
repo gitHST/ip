cd30bb9b69fdfa82d7f5e6bc469451a378d629df
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-01-31 00:57:22 +0800

    Level-0

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,10 +1,14 @@
+
 public class Duchess {
     public static void main(String[] args) {
+
         String logo = " ____              _\n"
                 + "|  _ \\ _   _  ____| |_    ___  ___  ___\n"
                 + "| | | | | | |/ ___|  _ \\ / _ \\/ __|/ __|\n"
                 + "| |_| | |_| | |___| | | |  __/\\__ \\\\__ \\\n"
                 + "|____/ \\__,_|\\____|_| |_|\\___||___/|___/\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("\nHello from\n" + logo + "\nHow may I serve you today?\n  ~Type -h for help~");
+
+        System.out.println("Goodbye! It was nice chatting.");
     }
 }

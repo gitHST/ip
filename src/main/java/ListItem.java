b13f5fd26f16e4442b9e64d25490a39755ff00d5
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-02-01 23:16:41 +0800

    Ensured all code complied with IntelliJ 2025 Coding Standard

--- src/main/java/ListItem.java
+++ src/main/java/ListItem.java
@@ -1,19 +1,24 @@
 public class ListItem {
     String itemText;
     boolean ticked;
+
     public ListItem(String itemText) {
         this.itemText = itemText;
         this.ticked = false;
     }
+
     public void tick() {
         this.ticked = true;
     }
+
     public void untick() {
         this.ticked = false;
     }
+
     public String toString() {
         return itemText;
     }
+
     public boolean isTicked() {
         return ticked;
     }

c2638d938fd8b451f77bf84849b4299bdc2f1681
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-02-10 22:03:56 +0800

    Added Exception handling

---
+++ src/main/java/exceptions/EmptyTodoDescriptionException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * Exception thrown when the description of a todo is empty.
+ */
+public class EmptyTodoDescriptionException extends DuchessException {
+    public EmptyTodoDescriptionException() {
+        super("OOPS!!! The description of a todo cannot be empty.");
+    }
+}
---
+++ src/main/java/exceptions/UnrecognizedCommandException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * Exception thrown when the user enters an unrecognized command.
+ */
+public class UnrecognizedCommandException extends DuchessException {
+    public UnrecognizedCommandException() {
+        super("OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+}

944815e0897ae6004e28e06e97426ea3f6a67707
Luke Williamson <l.williamson8@ncl.ac.uk>
2025-03-06 17:11:49 +0800

    Removed redundant code

--- src/main/java/duchess/Duchess.java
+++ src/main/java/duchess/Duchess.java
@@ -1,11 +1,9 @@
 package duchess;
 
-import duchess.cn4.ConnectFourGame;
 import duchess.exceptions.DuchessException;
 import duchess.exceptions.UnrecognizedCommandException;
 import duchess.list.ListManager;
 
-import java.util.Random;
 import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
@@ -19,7 +17,6 @@ public class Duchess {
             Here are some commands you can try:
               -h:         Display help
               quit:       Exit the chatbot
-              connect4:   Play a game of connect 4
               Lists:
                 Please use _ for spaces!
                 list {name}: Start or display a list named {name}
@@ -70,37 +67,7 @@ public class Duchess {
                 throw new DuchessException("An error occurred while processing your list command.");
             }
         }
-
-        if (lowerCaseInput.equals("connect4")) {
-            return handleConnectFourRequest();
-        }
-
         throw new UnrecognizedCommandException();
-    }
-
-    /**
-     * Handles the user request to play Connect Four.
-     *
-     * @return Response message indicating if the game can be played.
-     */
-    private String handleConnectFourRequest() {
-        if (IS_USING_TERMINAL) {
-            wantsToPlayCN4 = true;
-            return "You want to play connect four? Okay!\n\n";
-        }
-        return "I'm sorry dear, we simply can't play connect four in this environment! :( " +
-                "Please be a doll and run me in a terminal to play connect four.";
-    }
-
-    /**
-     * Returns a random invalid command response.
-     *
-     * @return A random response indicating unrecognized input.
-     */
-    private String getRandomInvalidResponse() {
-        Random random = new Random();
-        int index = random.nextInt(INVALID_COMMAND_RESPONSES.length);
-        return INVALID_COMMAND_RESPONSES[index] + "\n  ~Type -h for help~";
     }
 
     /**
@@ -132,10 +99,6 @@ public class Duchess {
                 Printer.printNicely("Duchess: " + duchess.getBotResponse(userInput));
             } catch (DuchessException e) {
                 Printer.printNicely("Duchess: " + e.getMessage());
-            }
-
-            if (duchess.wantsToPlayCN4) {
-                launchConnectFour(duchess);
             }
         }
         scanner.close();
@@ -179,22 +142,5 @@ public class Duchess {
         sleep(300);
         Printer.printLogo(5, logo, 20);
         Printer.printNicely("\nHow may I serve you today?\n  ~Type -h for help~\n", 3);
-    }
-
-    /**
-     * Launches the Connect Four game.
-     *
-     * @param duchess The chatbot instance.
-     * @throws InterruptedException If thread sleep is interrupted.
-     */
-    private static void launchConnectFour(Duchess duchess) throws InterruptedException {
-        Printer.printNicely("Loading");
-        for (int i = 0; i < 6; i++) {
-            System.out.print(".");
-            sleep(500);
-        }
-        boolean won = new ConnectFourGame().playGame();
-        duchess.wantsToPlayCN4 = false;
-        Printer.printNicely("Duchess: " + (won ? "You won!" : "Oh no, you lost!") + "\n  ~Type -h for help~");
     }
 }
